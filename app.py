d1a23bca65c03000bad4d85800ab70d7629303f4
Arkadiusz Gasecki <arkadiusz.gasecki@gmail.com>
2021-05-23 18:50:08 +0200

    update

--- app.py
+++ app.py
@@ -3,11 +3,17 @@ import streamlit.components.v1 as components
 import pandas as pd
 import io, base64
 import requests, json
+import configparser
+
 
 import SessionState
 
+from streamlit import script_runner
+
+
 # define session variables
-session_state = SessionState.get(src_token=None, src_url=None, src_objects=None, tgt_token=None, tgt_url=None, tgt_objects=None)
+session_state = SessionState.get(src_token=None, src_url=None, src_objects=None, tgt_token=None, tgt_url=None, tgt_objects=None, autocomplete=None, added=False, add_cols=list(),all_columns=list())
+
 
 # define default style
 st.markdown(
@@ -81,31 +87,38 @@ def login(url, consumer_key, consumer_secret, username, password, org='src'):
 			session_state.tgt_token = response['access_token']
 			session_state.tgt_url = url
 		
-
+def add_new_cols(new_cols):
+	session_state.add_cols = new_cols
 
 ########### load objects ##############
-def load_objects():
-	if session_state.token is not None:
+def load_objects(org='src'):
+
+	token = session_state.src_token if org == 'src' else session_state.tgt_token
+	url = session_state.src_url if org == 'src' else session_state.tgt_url
+	if token is not None:
 		#get list of all objects
-		request_url = session_state.url + 'services/data/v51.0/sobjects'
+		request_url = url + 'services/data/v51.0/sobjects'
 		header = {
-			'Authorization': 'Bearer '+session_state.token
+			'Authorization': 'Bearer '+token
 		}
 		obj_metadata_response = requests.get(request_url, headers=header)
 		response = json.loads(obj_metadata_response.text)
 		
 		dct = {}
 		for obj in response['sobjects']:
-			dct[obj['name']] = obj['label']
-		session_state.objects = dct
+			dct[obj['label']] = obj['name']
+		return dct
 	else:
-		st.write('Not connected') 
+		st.write('Not connected')
 
 
 ########### describe object ##############
 
 def parse_response(response, params,key='fields'):
 	d = {}
+	additional_columns = list()
+	if key == 'fields':
+		additional_columns = list(response[key][0].keys())
 
 	for r in response[key]:
 		for p in params:
@@ -114,7 +127,7 @@ def parse_response(response, params,key='fields'):
 				d[p].append(r[p])
 			else:
 				d[p].append( ';'.join([ '"{}"'.format(e['value']) for e in r[p] ]) )
-	return d
+	return (d, additional_columns)
 
 def prepare_html_table(d, length, params):
 	html = '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">'
@@ -128,14 +141,14 @@ def prepare_html_table(d, length, params):
 	for i in range(0,length):
 		html += '<tr>'
 		for p in params:
-			if p == 'picklistValues':
-				if len(d[p][i]) > 0:
+			if p.startswith('picklistValues'):
+				if isinstance(d[p][i],str) and len(d[p][i]) > 0:
 					elem = '<select style="width: 200px">'+"".join(['<option>'+e.replace('"','')+'</option>' for e in d[p][i].split(';')])+'</select>'
 				else:
 					elem = ''
 				html += ('<td>'+elem+'</td>')
-			elif p == 'referenceTo':
-				if len(d[p][i]) > 0:
+			elif p.startswith('referenceTo'):
+				if isinstance(d[p][i],list) and len(d[p][i]) > 0:
 					html += ('<td>'+str(d[p][i][0])+'</td>')
 				else:
 					html += ('<td></td>')
@@ -147,55 +160,96 @@ def prepare_html_table(d, length, params):
 
 	return html
 
-def show_object(object_name):
+def show_object(object_name,org='src',add=list()):
 	# get metadata info about desired object
-	request_url = session_state.url + 'services/data/v51.0/sobjects/'+object_name+'/describe'
+	token = session_state.src_token if org == 'src' else session_state.tgt_token
+	url = session_state.src_url if org == 'src' else session_state.tgt_url
+
+	request_url = url + 'services/data/v51.0/sobjects/'+object_name+'/describe'
 	header = {
-		'Authorization': 'Bearer '+session_state.token
+		'Authorization': 'Bearer '+token
 	}	
 	obj_metadata_response = requests.get(request_url, headers=header)
 	response = json.loads(obj_metadata_response.text)
 	
 	#parse response about object
-	params = ['name', 'label', 'type', 'length', 'nillable', 'referenceTo' ,'picklistValues']
-
-	d = parse_response(response, params)
+	params = ['name', 'label', 'type', 'length', 'nillable', 'referenceTo' ,'picklistValues'] + add
+	
+	d, additional_columns = parse_response(response, params)
+	df = pd.DataFrame(d)
+	
 	object_structure = prepare_html_table(d, len(d['name']), params)
 
 	#get metadata info about validation rules
-	request_url = session_state.url + "services/data/v51.0/tooling/query?q=Select Id,Active,Description,ErrorDisplayField,ErrorMessage From ValidationRule Where EntityDefinition.DeveloperName = '"+object_name+"'"
+	request_url = url + "services/data/v51.0/tooling/query?q=Select Id,Active,Description,ErrorDisplayField,ErrorMessage From ValidationRule Where EntityDefinition.DeveloperName = '"+object_name+"'"
 	header = {
-		'Authorization': 'Bearer '+session_state.token
+		'Authorization': 'Bearer '+token
 	}	
 	obj_metadata_response = requests.get(request_url, headers=header)
 	response = json.loads(obj_metadata_response.text)
-
+	
 	params = ['Id', 'Active', 'Description', 'ErrorDisplayField', 'ErrorMessage' ]
 	
-	d = parse_response(response, params,key='records')
-	validation_rules = prepare_html_table(d, len(d['Id']), params)
+	d, _ = parse_response(response, params,key='records')
+	validation_rules = None if len(d) == 0 else prepare_html_table(d, len(d['Id']), params)
 
-	return (object_structure, validation_rules)
-
-
+	return (object_structure, validation_rules, additional_columns, df)
+	
+def get_table_download_link_csv(df,filename):
+    csv = df.to_csv().encode()
+    b64 = base64.b64encode(csv).decode()
+    href = f'<a href="data:file/csv;base64,{b64}" download="'+filename+'.csv" target="_blank">Download csv file</a>'
+    return href
 
 ######################### main site ###################################
 
 # add code that allow to autocomplete values read from config.ini file
-cached_src_url = ''
-cached_src_user = ''
-cached_src_password = ''
-cached_src_key = ''
-cached_src_secret = ''
 
-cached_tgt_url = ''
-cached_tgt_user = ''
-cached_tgt_password = ''
-cached_tgt_key = ''
-cached_tgt_secret = ''
+if st.button('Autocomplete',key='autocomplete') or session_state.autocomplete:
+	config = configparser.ConfigParser()
+	config.read('config.ini')
 
-if st.button('Autocomplete',key='autocomplete'):
-	user = 'username'
+	for key, value in config['SOURCE CONNECTION'].items():
+		if key == 'url':
+			cached_src_url = value
+		elif key == 'client_id':
+			cached_src_key = value
+		elif key == 'client_secret':
+			cached_src_secret = value
+		elif key == 'username':
+			cached_src_user = value
+		elif key == 'password':
+			cached_src_password = value
+		elif key == 'token':
+			cached_src_password += value
+
+	for key, value in config['TARGET CONNECTION'].items():
+		if key == 'url':
+			cached_tgt_url = value
+		elif key == 'client_id':
+			cached_tgt_key = value
+		elif key == 'client_secret':
+			cached_tgt_secret = value
+		elif key == 'username':
+			cached_tgt_user = value
+		elif key == 'password':
+			cached_tgt_password = value
+		elif key == 'token':
+			cached_tgt_password += value
+	session_state.autocomplete = True
+
+if session_state.autocomplete is None:
+	cached_src_url = ''
+	cached_src_user = ''
+	cached_src_password = ''
+	cached_src_key = ''
+	cached_src_secret = ''
+
+	cached_tgt_url = ''
+	cached_tgt_user = ''
+	cached_tgt_password = ''
+	cached_tgt_key = ''
+	cached_tgt_secret = ''
 
 src_col, tgt_col = st.beta_columns(2)
 
@@ -209,7 +263,7 @@ with src_col:
 	src_submit_connect = src_form.form_submit_button(label='Login')
 
 	if st.button('Load source objects',key='load_src_obj'):
-		load_objects()
+		session_state.src_objects = load_objects()
 
 with tgt_col:
 	tgt_form = st.form(key='conn_form_20')
@@ -221,35 +275,91 @@ with tgt_col:
 	tgt_submit_connect = tgt_form.form_submit_button(label='Login')
 	
 	if st.button('Load target objects',key='load_tgt_obj'):
-		load_objects()
+		session_state.tgt_objects = load_objects('tgt')
 
 if src_submit_connect:
 	login(src_url, src_consumer_key, src_consumer_secret, src_username, src_password, org='src')
 
+if tgt_submit_connect:
+	login(tgt_url, tgt_consumer_key, tgt_consumer_secret, tgt_username, tgt_password, org='tgt')
 
 
 
 ######################### sidebar ###################################
 
 if session_state.src_token is None:
-	object_selection = st.sidebar.empty()
+	src_object_selection = st.sidebar.empty()
 else:
-	object_selection = st.sidebar.selectbox('Select object', ('') if session_state.objects is None else list(session_state.objects.values()))
+	src_object_selection = st.sidebar.selectbox('Select source object', ('') if session_state.src_objects is None else list(session_state.src_objects.keys()))
+
+if session_state.tgt_token is None:
+	tgt_object_selection = st.sidebar.empty()
+else:
+	tgt_object_selection = st.sidebar.selectbox('Select target object', ('') if session_state.tgt_objects is None else list(session_state.tgt_objects.keys()))
+
+
 
 ## get info about object
-if st.sidebar.button('Show object') and session_state.token is not None:
-	
-	(object_structure, validation_rules) = show_object(session_state.objects[object_selection.strip()])
 
-	st.markdown('## Table structure ##')
-	components.html(object_structure,height=500,scrolling=True)
+object_structure = ''
+validation_rules = ''
+org_selected = ''
+df = pd.DataFrame()
+filename = 'capture'
+if (st.sidebar.button('Show source object',key='show_src') or session_state.added) and session_state.src_token is not None:
+	(object_structure, validation_rules, additional_columns, df) = show_object(session_state.src_objects[src_object_selection.strip()],org='src',add=session_state.add_cols)
+	session_state.all_columns = additional_columns
+	session_state.added = False
+	org_selected = 'Source'
+	filename = session_state.src_objects[src_object_selection.strip()]
 	
+	
+if st.sidebar.button('Show target object',key='show_tgt' or session_state.added) and session_state.tgt_token is not None:
+	(object_structure, validation_rules, additional_columns, df) = show_object(session_state.tgt_objects[tgt_object_selection.strip()],org='tgt',add=session_state.add_cols)
+	session_state.all_columns = additional_columns
+	session_state.added = False
+	org_selected = 'Target'
+	filename = session_state.tgt_objects[tgt_object_selection.strip()]
+
+if st.sidebar.button('Compare objects', key='show_cmp'):
+	session_state.added = False
+	org_selected = 'Compare'
+	(_1,_2,_3, sdf) = show_object(session_state.src_objects[src_object_selection.strip()],org='src',add=session_state.add_cols)
+	(_1,_2,_3, tdf) = show_object(session_state.tgt_objects[tgt_object_selection.strip()],org='tgt',add=session_state.add_cols)
+	
+	sdf = sdf.merge(tdf,how='outer',left_on='name', right_on='name',suffixes=('_src','_tgt'))
+	st.markdown(get_table_download_link_csv(sdf,session_state.src_objects[src_object_selection.strip()]), unsafe_allow_html=True)
+	
+	html = prepare_html_table(sdf.to_dict(), len(sdf), list(sdf.columns)) 
+	components.html(html,height=500,scrolling=True)
+	
+
+if org_selected != 'Compare':
+	st.markdown('## '+org_selected+' Table structure ##')
+	st.markdown(get_table_download_link_csv(df,filename), unsafe_allow_html=True)
+	components.html(object_structure,height=500,scrolling=True)
+
+## add form for adding columns
+col_form = st.form(key='col_form')
+new_cols = col_form.multiselect('Select additional columns', session_state.all_columns)
+sub = col_form.form_submit_button('Add')
+if sub:
+	session_state.added = True
+	if new_cols != session_state.add_cols:
+		add_new_cols(new_cols)
+		raise st.script_runner.RerunException(st.script_request_queue.RerunData(None))
+
+
+## show validation rules
+if org_selected != 'compare':
 	st.markdown('## Validation rules ##')
 	components.html(validation_rules,height=500,scrolling=True)
 
+## add logout
 
 if st.sidebar.button('Logout', key='logout'):
-	logout(org='src')	
+	logout(org='src')
+	logout(org='tgt')
 
 
 
